ac844bdbb85d1803e1b97d7d947fdc245e0ef76d
MaheshB77 <mbansode7696@gmail.com>
2026-01-03 22:39:57 +0530

    Added widget

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Mahesh Bansode on 03/01/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
